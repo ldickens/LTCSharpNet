8bd1d84685ed97e892c7db6309e7fd85f1b48af1
LeonDickensGH <leon.dickens@green-hippo.com>
2025-09-17 22:40:57 +0100

    Implementing AB ring buffers and testing

--- TestEncoder/Program.cs
+++ TestEncoder/Program.cs
@@ -1,11 +1,121 @@
 ﻿using NAudio;
 using NAudio.Utils;
 using NAudio.Wave;
+using System.Collections.Immutable;
+using System.ComponentModel.Design;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace TestEncoder
 {
+    public class TCFrameBufferManager
+    {
+        byte[,] m_frameBuffers;
+        TCRingBuffer m_activeBuffer;
+        int m_activeBufferIndex = 0;
+        int m_numOfFrameBuffers = 0;
+        int m_activeBufferDataIndex = 0;
+        int m_activeBufferDataNext = 0;
+        const int m_sizeOfFrameBuffer = 1600;
+
+        public TCRingBuffer ActiveBuffer => m_activeBuffer;
+
+        public TCFrameBufferManager(int numOfFrames)
+        {
+            m_numOfFrameBuffers = numOfFrames;
+            m_frameBuffers = new byte[m_numOfFrameBuffers, m_sizeOfFrameBuffer];
+
+            byte[] tmp = new byte[m_sizeOfFrameBuffer];
+            Buffer.BlockCopy(m_frameBuffers, 0, tmp, 0, numOfFrames);
+            m_activeBuffer = new(m_sizeOfFrameBuffer, tmp);
+        }
+
+        private byte[] GetTCFrameBuffer(int index)
+        {
+            byte[] tmp = new byte[m_sizeOfFrameBuffer];
+            for (int i = 0; i < m_sizeOfFrameBuffer; i++)
+            {
+                tmp[i] = m_frameBuffers[index, i];
+            }
+            return tmp;
+        }
+
+        public void IncrementActiveBuffer()
+        {
+            m_activeBufferIndex++;
+            m_activeBuffer.Write(GetTCFrameBuffer(m_activeBufferIndex));
+        }
+
+        public void FillSingularBuffer(int index, byte[] source)
+        {
+            Buffer.BlockCopy(source, 0, m_frameBuffers, index * m_numOfFrameBuffers, m_sizeOfFrameBuffer);
+            if (m_activeBufferIndex == index)
+            {
+                m_activeBuffer.Write(GetTCFrameBuffer(m_activeBufferIndex));
+            }
+        }
+
+        public byte[] ReadActiveBuffer(int size)
+        {
+            byte[] tmp = m_activeBuffer.Read(size);
+            Buffer.BlockCopy(tmp, 0, tmp, 0, tmp.Length);
+            return tmp;
+        }
+    }
+
+    public class TCRingBuffer
+    {
+        private readonly byte[] m_buffer;
+        private int m_tail; // next read position
+        private const int m_maxSize = 1600; // size of a singular frame of smpte timecode data
+        private object m_lock = new();
+
+        public int Capacity { get; }
+
+        public TCRingBuffer(int capacity, byte[] source)
+        {
+            if (capacity <= 0)
+                throw new ArgumentException("Capacity must be greater than zero.", nameof(capacity));
+
+            Capacity = capacity;
+            m_buffer = source;
+            m_tail = 0;
+        }
+
+        public void Write(byte[] source)
+        {
+            Debug.Assert(source.Length == m_maxSize, $"Timecode frame data provided is not a valid length of {m_maxSize}");
+            lock (m_lock)
+            {
+                Buffer.BlockCopy(source, 0, m_buffer, 0, source.Length);
+            }
+        }
+
+        public byte[] Read(int size)
+        {
+            lock (m_lock)
+            {
+                int count = 0;
+                int tmpHead = 0;
+                byte[] tmp = new byte[size];
+                while (count < size)
+                {
+                    int chunkSize = m_buffer.Length - m_tail;
+                    if (chunkSize > (size - count))
+                    {
+                        chunkSize = (size - count);
+                    }
+                    Buffer.BlockCopy(m_buffer, m_tail, tmp, tmpHead, chunkSize);
+                    // figure out the m_tail updating position.
+                    count += chunkSize;
+                    tmpHead = count - 1;
+                }
+                return tmp;
+            }
+        }
+    }
+
     class Source : IWaveProvider
     {
         LTCSharpNet.Encoder m_encoder;
@@ -16,7 +126,7 @@ namespace TestEncoder
         long m_frametime = 0;
         Stopwatch m_timer = new();
         //Stopwatch m_timer2 = new();
-        const int m_bufSize = 1600*30;
+        const int m_bufSize = 1600 * 60;
         CircularBuffer m_circularBuffer = new(m_bufSize);
 
         public Source(long fps)
@@ -28,35 +138,38 @@ namespace TestEncoder
 
         public void StartEncoding()
         {
-            Task.Run(() =>
+            byte[] tmp = new byte[1600];
+            m_encoder.getBuffer(tmp, 0);
+            m_circularBuffer.Write(tmp, 0, tmp.Length);
+            m_timer.Start();
+
+
+            _ = Task.Run(() =>
             {
-                byte[] tmp = new byte[1600];
+
+                int now = m_timer.Elapsed.Milliseconds;
+                int lastFrame = m_timer.Elapsed.Milliseconds;
 
                 while (true)
                 {
-                    if (m_circularBuffer.MaxLength == m_circularBuffer.Count)
-                    {
-                        //Thread.Sleep(2);
-                        continue;
-                    }
+                    now = m_timer.Elapsed.Milliseconds;
+                    int delta = now - lastFrame;
+                    lastFrame = now;
 
-                    if (!m_encoding)
-                    {
-                        m_timer.Start();
-                        //m_timer2.Start();
-                        m_encoding = true;
-                        m_encoder.getBuffer(tmp, 0);
-                    }
-                    else if (m_timer.ElapsedTicks > m_frametime)
+                    if (m_timer.ElapsedTicks > m_frametime)
                     {
 
                         m_timer.Restart();
-                        m_circularBuffer.Reset();
                         m_encoder.incrementFrame();
                         m_encoder.encodeFrame();
 
                         m_encoder.getBuffer(tmp, 0);
-                        //m_currentTC = m_encoder.getTimecode().ToString();
+                        m_circularBuffer.Reset();
+                        //Console.WriteLine("UpdatedFrame");
+                    }
+                    else if (delta < 33)
+                    {
+                        Thread.Sleep(33 - delta);
                     }
                     m_circularBuffer.Write(tmp, 0, tmp.Length);
                 }
@@ -113,28 +226,16 @@ namespace TestEncoder
             int fps = 30;
             var waveOut = new WaveOutEvent() { DeviceNumber = 10 };
             var encoder = new Source(fps);
-            waveOut.DesiredLatency = 40; 
+            waveOut.DesiredLatency = 40;
             waveOut.Init(encoder);
             encoder.SetTimecode(new LTCSharpNet.Timecode(0, 0, 0, 0));
             encoder.StartEncoding();
-            Thread.Sleep(100);
-			waveOut.Play();
-
-            _ = Task.Run(() =>
-            {
-                Stopwatch timer = new();
-                timer.Start();
-                while (true)
-                {
-                    Console.WriteLine(timer.Elapsed);
-                    Thread.Sleep(1000);
-                }
-            });
+            waveOut.Play();
 
             while (true) { }
 
-			waveOut.Stop();
-			waveOut.Dispose();
+            waveOut.Stop();
+            waveOut.Dispose();
         }
     }
 }
---
+++ UnitTests/EncoderTesting.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using TestEncoder;
+
+namespace EncoderTests 
+{
+    [TestClass]
+    public sealed class EncoderBufferTesting
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var bufManager = new TCFrameBufferManager(100);
+            byte[] tmp = new byte[1600];
+
+            int count = 0;
+            while (count < tmp.Length)
+            {
+                for (byte i = 0; i < 255;  i++)
+                {
+                    tmp[count] = i;
+                    count++;
+                    
+                    if (count == tmp.Length) { break; }
+                }
+
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                bufManager.FillSingularBuffer(i, tmp);
+            }
+
+            TCRingBuffer active = bufManager.ActiveBuffer;
+            byte[] firstArray = active.Read(3200);
+            byte[] secondArray = active.Read(300);
+
+            Assert.IsTrue(SequentialFrameTesting(firstArray, 0));
+
+            int secondArrayStartingNum = 3200 % 255;
+
+            Assert.IsTrue(SequentialFrameTesting(secondArray, secondArrayStartingNum));
+
+        }
+
+        static private bool SequentialFrameTesting(byte[] testArray, int startNum)
+        {
+            // check that the arrays have consecutive numbers up to 255
+            int controlInt = startNum > 0 ? startNum : 0;
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                Trace.WriteLine($"index{i} = {testArray[i]}");
+                Assert.IsTrue(i == controlInt);
+                if (i == controlInt)
+                {
+                    if (i == 255)
+                    {
+                        controlInt = 0;
+                        continue;
+                    }
+
+                    controlInt++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
